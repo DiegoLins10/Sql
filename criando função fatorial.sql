2ae4fe7e749ed3289202ef3b3fb39fe689bbca50
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-15 21:07:12 -0300

    Criando função fatorial sql server

---
+++ criando função fatorial.sql
@@ -0,0 +1,18 @@
+-- CRIAÇÃO FUNÇÃO FATORIAL SQL SERVER
+CREATE FUNCTION dbo.fatorial ( @numero int )
+RETURNS int  
+AS 
+BEGIN 
+ If(@numero <= 1)
+	return 1
+  return @numero * dbo.fatorial(@numero-1) -- usando recursividade
+END
+go
+
+--Função chamando a função fatorial usando print
+Declare @n int = 1;
+WHILE (@n <= 10)
+begin
+print CONVERT(VARCHAR,@n)+'! = '+CONVERT(VARCHAR,dbo.fatorial(@n)); -- USAR O CONVERT PQ O FATORIAL NÃO É STRING
+SET @n = @n + 1;
+END;
