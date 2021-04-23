a900db0bb91453228a0f8b45965e46723640c2e4
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-23 17:59:14 -0300

    insert com select

---
+++ insert com select.sql
@@ -0,0 +1,9 @@
+ --insert processo.DominioParametroProceso
+ select 
+138 IdParametroProcesso
+,Nome 
+,Valor
+ from processo.DominioParametroProceso
+ where idparametroprocesso = 45
+
+ ------ inserir varios valores com insert select
