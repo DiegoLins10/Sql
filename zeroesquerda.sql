cd073df04721862f5e6b5de912db4fe156adb4e0
Diego Lins <diegofernandeslins@gmail.com>
2022-08-10 09:06:19 -0300

    Create zeroesquerda.sql

---
+++ zeroesquerda.sql
@@ -0,0 +1,11 @@
+0 a esquerda
+
+dicas cleiton
+
+ah man, se vc ver campos de CEP, coloca sempre isso aqui:
+
+right(replicate('0',8) + fu.CEP,8)
+
+pra garantir 0 a esquerda
+
+e se for cpf, pode usar o mesmo codigo, só troca de 8 pra 11
